b6517223a6ad737e0aa30f84aae1c74b09a84d50
SonokaOsawa <sooonokaaa@gmail.com>
2021-10-20 18:11:38 +0900

    firebase 修正

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBMe7LGPSa0GFyqfOuvMXhNhEY45IzWx58",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    authDomain: "ec-next.firebaseapp.com",
+    projectId: "ec-next",
+    storageBucket: "ec-next.appspot.com",
+    messagingSenderId: "173383077266",
+    appId: "1:173383077266:web:122f71614dcb95d100454b",
+    measurementId: "G-J0R7GPPPNX"
 };
 
 if (firebase.apps.length === 0) {
