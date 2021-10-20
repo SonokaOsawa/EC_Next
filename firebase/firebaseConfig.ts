66e198f28c6167fc2d2603019bb1811372775368
SonokaOsawa <sooonokaaa@gmail.com>
2021-10-20 18:02:53 +0900

    firebase apikey 修正

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: "AIzaSyBMe7LGPSa0GFyqfOuvMXhNhEY45IzWx58",
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
